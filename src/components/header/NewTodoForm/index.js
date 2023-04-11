f50d169646cabf4b07992859209dd24d0c27e66b
anilcosarss <anilcosarsg12@gmail.com>
2023-04-11 13:42:54 +0300

    first commit

---
+++ src/components/header/NewTodoForm/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Formik, Field, Form } from 'formik';
+import validationsSchema from './validations';
+import { useTodo } from '../../../contexts/TodoContext';
+
+
+function NewTodoForm() {
+  const{addTodo} = useTodo();
+
+  return (
+    <Formik
+      initialValues={{
+        text: '',
+
+      }}
+      onSubmit={(values,bag) => {      
+        console.log(values);
+        
+        addTodo(values.text);
+
+        bag.resetForm();
+
+
+      }}
+      validationSchema={validationsSchema}
+
+    >
+      <Form>
+        <Field
+          className="new-todo"
+          placeholder="What needs to be done?"
+          autoFocus
+          id="text"
+          name="text" />
+      </Form>
+    </Formik>
+  )
+}
+
+export default NewTodoForm;
---
+++ src/components/header/index.js
@@ -0,0 +1,15 @@
+import { Form } from 'formik';
+import React from 'react';
+import NewTodoForm from './NewTodoForm';
+
+
+function Header() {
+  return (
+    <header className="header">
+		<h1>todos</h1>
+		<NewTodoForm/>
+	</header>
+  )
+}
+
+export default Header;
